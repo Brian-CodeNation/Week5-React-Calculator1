e44574c67cfbe42c98ae4182346863c3d3b9808a
Brian Harkins <brian.harkins@wearecodenation.com>
2023-08-31 12:14:36 +0100

    Added js and css file with working calculator and simple styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [sum, setSum] = useState("");
+  const buttons = ["7", "8", "9", "C", "4", "5", "6", "x", "1", "2", "3", "/", "+", "0", "-", "="];
+
+  const handleClick = (string) => {
+    if (string === "="){
+      let expression = sum;
+      setSum(evaluate(expression))
+    } else if (string === "C") {
+      setSum("")
+    } else {
+      setSum(sum + string)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{sum}</h1>
+      <div className='buttonWrap'>
+        {
+          buttons.map((button, index) => {
+            return (
+              <button onClick={() => handleClick(button)} key={index}>{button}</button>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
